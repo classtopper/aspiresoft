fe04caef92881c786c62f8c9caeeffc60ea35587
Arvind Kumar <arvind.pravat@gmail.com>
2020-06-07 13:22:15 +0200

    Initial Draft

---
+++ assets/js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Please enter your name.",
+                    minlength: "Name must be at least 2 characters."
+                },
+                subject: {
+                    required: "Please enter the subject.",
+                    minlength: "Subject must be at least 4 characters."
+                },
+                number: {
+                    required: "Please enter number.",
+                    minlength: "Number must be at least 5 characters"
+                },
+                email: {
+                    required: "Please enter your name."
+                },
+                message: {
+                    required: "Please enter message.",
+                    minlength: "Message must be at least more than 20 characters."
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contactServiceUrl",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
